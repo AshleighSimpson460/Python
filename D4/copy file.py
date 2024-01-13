2912bcc5b82da612036f22147ef01f021627f4ba
Ashleigh <118854789+AshleighSimpson460@users.noreply.github.com>
2024-01-13 23:48:49 +0000

    Delete D4/copy file.py

--- D4/copy file.py
+++
@@ -1,9 +0,0 @@
-# copyfile() = copies contents of a file
-# copy() = copyfile() + permission mode + destination can be a directory
-# copy2() = copy() + copies metadata (file's creation and modification times)
-
-import shutil
-
-shutil.copyfile('c:\\Users\Ashleigh\\Desktop\\text.txt','c:\\Users\Ashleigh\\Desktop\\text_copy.txt')
-shutil.copy2('c:\\Users\Ashleigh\\Desktop\\text.txt','c:\\Users\Ashleigh\\Documents\\')
-
